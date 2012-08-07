923f7dd03b91712bbb43f5efe79d84c95b666749
shelling <navyblueshellingford@gmail.com>
2012-08-07 13:13:48 +0800

    implement Tag.pm

---
+++ lib/XLIFF/Object/Tag.pm
@@ -0,0 +1,119 @@
+package XLIFF::Object::Tag;
+
+use Moose;
+
+use XML::Simple;
+
+around BUILDARGS => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    my %args = @_;
+    my ($name, $content) = delete @args{qw(name content)};
+
+    $self->$orig(
+        name    => $name,
+        content => $content,
+        attr    => {%args},
+    );
+};
+
+has name => (
+    is  => "rw",
+    isa => "Str",
+);
+
+has attr => (
+    is      => "rw",
+    isa     => "HashRef",
+    traits  => ['Hash'],
+    handles => {
+        set     => 'set',
+        get     => 'get',
+    },
+);
+
+has content => (
+    is  => "rw",
+);
+
+sub to_xml {
+    my ($self, ) = @_;
+    XMLout({
+        %{$self->attr},
+        content => $self->content,
+    }, RootName => $self->name);
+}
+
+sub from_xml {
+    my ($class, $xml) = @_;
+    my ($name, $hash) = %{XMLin(
+        $xml,
+        ForceContent => 1,
+        KeepRoot     => 1,
+    )};
+    $class->new(
+        name => $name,
+        %{$hash},
+    );
+}
+
+sub from_perl {
+    my ($class, $name, $hash) = @_;
+    $class->new(
+        name => $name,
+        %{$hash},
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
+=pod
+
+=head1 NAME
+
+XLIFF::Object::Tag - 
+
+=head1 SYNOPSIS
+
+
+
+=head1 DESCRIPTION
+
+This class define the conversion from xml and representation of a xml tag in XLIFF.
+Consider a xml fragment as following.
+
+    <tagname attr1="attr1" attr2="attr2">
+        content string
+    </tagname>
+
+Invoking from_xml() would internally convert it as the perl data structure as following.
+
+    tagname => {
+        attr1   => "attr1",
+        attr2   => "attr2",
+        content => "content string",
+    }
+
+Then the you can retrive attributes.
+
+    $tag->name            #=> "tagename"
+    $tag->get('attr1')    #=> "attr1"
+    $tag->get('attr2')    #=> "attr2"
+    $tag->content         #=> "content string"
+
+Using to_xml() to generate the same xml fragment
+
+    my $tag = XLIFF::Object::Tag->new(
+        name    => "tagname",
+        content => "content string",
+        attr1   => "attr1",
+        attr2   => "attr2",
+    );
+
+    $tag->to_xml;
+
+=cut
---
+++ t/object/tag.t
@@ -0,0 +1,84 @@
+use Modern::Perl;
+use Test::More;
+use XLIFF::Object::Tag;
+
+my $tag = XLIFF::Object::Tag->new(
+    name       => "source",
+    content    => "test content",
+    "xml:lang" => "en",
+);
+
+$tag->set(info => "zh");
+
+is (
+    $tag->get("xml:lang"),
+    "en",
+    "constructor can set",
+);
+
+is (
+    $tag->get("info"),
+    "zh",
+    "set() can set",
+);
+
+is (
+    $tag->content,
+    "test content",
+    "can access content",
+);
+
+is (
+    $tag->to_xml,
+    qq{<source info="zh" xml:lang="en">test content</source>\n},
+    "to_xml() ok",
+);
+
+my $from_xml = XLIFF::Object::Tag->from_xml(
+    '<target info="info">content</target>'
+);
+
+is (
+    $from_xml->name,
+    "target",
+    "from_xml() can set tag name",
+);
+
+is (
+    $from_xml->get("info"),
+    "info",
+    "from_xml() can set attr",
+);
+
+is (
+    $from_xml->content,
+    "content",
+    "from_xml() can set tag content",
+);
+
+my $from_perl = XLIFF::Object::Tag->from_perl(
+    source => {
+        "xml:lang" => "en",
+        "content"  => "source string",
+    }
+);
+
+is (
+    $from_perl->name,
+    "source",
+    "from_perl() can set name",
+);
+
+is (
+    $from_perl->content,
+    "source string",
+    "from_perl() can set content",
+);
+
+is (
+    $from_perl->get("xml:lang"),
+    "en",
+    "from_perl() can set attr",
+);
+
+done_testing;
