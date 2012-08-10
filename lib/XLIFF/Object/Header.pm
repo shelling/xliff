7c022c52a0199a0ca046229ccfcea80421be7d3c
shelling <navyblueshellingford@gmail.com>
2012-08-10 13:38:44 +0800

    add skeleton of Header.pm

---
+++ lib/XLIFF/Object/Header.pm
@@ -0,0 +1,39 @@
+package XLIFF::Object::Header;
+
+use Moose;
+
+sub to_perl {
+    my ($self, ) = @_;
+    return (
+        header => {
+        }
+    );
+}
+
+sub from_perl {
+    my ($class, $hash) = @_;
+    $class->new();
+}
+
+sub to_xml {
+    my ($self, ) = @_;
+
+}
+
+sub from_xml {
+    my ($self, ) = @_;
+
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
+=pod
+
+=head1 NAME
+
+XLIFF::Object::Header - header section in XLIFF
+
+=cut
---
+++ t/object/header.t
@@ -0,0 +1,6 @@
+use Modern::Perl;
+use Test::More;
+
+ok 1;
+
+done_testing;
